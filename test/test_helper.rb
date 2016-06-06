039f5b7da6eaaf2ad3ee493fd863d4ce9eeec8b6
rob pennock <robpennock@gmail.com>
2016-06-06 15:35:53 -0700

    testing for title works fully. Added layouts with linking titles

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require "minitest/reporters"
+require "minitest/reporters" #next 2 lines add some helpful graphics in testing terminal
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
