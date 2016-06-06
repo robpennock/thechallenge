5772ffceee1f83bd7e3c2e340ae44ea9fc50a9a0
rob pennock <robpennock@gmail.com>
2016-06-06 14:03:49 -0700

    basic tests and static pages created

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -4,16 +4,19 @@ class MainControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    #assert_select "title", "Home | The Challenge"
   end
 
   test "should get music" do
     get :music
     assert_response :success
+    #assert_select "title", "Music | The Challenge"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    #assert_select "title", "About | The Challenge"
   end
 
 end
