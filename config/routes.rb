1c5df17602195ed36ecc1d52b58ae30b82bb97e1
rob pennock <robpennock@gmail.com>
2016-06-06 17:52:39 -0700

    bootstrap added. Routes updated. New testing procedures for routes. almost done with fonts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'main#home'
 
-  get 'main/music'
+  get 'home' => 'main#home'
 
-  get 'main/about'
+  get 'music' => 'main#music'
+
+  get 'about' => 'main#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -2,21 +2,38 @@ require 'test_helper'
 
 class MainControllerTest < ActionController::TestCase
   test "should get home" do
+    #test if home view exists
     get :home
     assert_response :success
+
+    #test title on page
     assert_select "title", "Home | The Challenge"
+
+    #test home route
+    assert_generates "/", :controller => "main", :action => 'home' 
   end
 
   test "should get music" do
+    #test if music view exists
     get :music
     assert_response :success
+
+    #test title on page
     assert_select "title", "Music | The Challenge"
+
+    #test music route
+    assert_generates "/music", :controller => "main", :action => "music"
   end
 
   test "should get about" do
+    #test if about view exists
     get :about
     assert_response :success
-    assert_select "title", "About | The Challenge"
-  end
 
+    #test title on page
+    assert_select "title", "About | The Challenge"
+
+    #test about route
+    assert_generates "/about", :controller => "main", :action => "about"
+  end
 end
