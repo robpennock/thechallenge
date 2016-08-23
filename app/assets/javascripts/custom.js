05fadba60d024de9dc06d66e443fa38e06026ea8
rob pennock <robpennock@gmail.com>
2016-08-23 14:10:54 -0700

    bootstrap functionality added. added js to support lyrics fading in and out on the music page

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,153 @@
+
+$(document).ready(function() {
+    $('#ssdy-btn').click(function() {
+        $('#mid-music').fadeOut(800, function(){
+        	$('#mid-music').empty().append("<h1>Same Shit Different Year</h1>" +
+	        	"<p>Thar be no lyrics here. Go watch the Twilight Zone episode <i>Maple Street</i></p>").fadeIn(1000);
+		});
+    });
+
+    $('#air-btn').click(function() {
+        $('#mid-music').fadeOut(800, function(){
+        	$('#mid-music').empty().append("<h1>As I Remember</h1>" +
+	        	"<p>As I sit and try, to remember all the things I wanted to convey to you, I come up blank.</p>" +
+	        	"<p>'Cause seeing you in front of me, has caused me to go numb I've forgotten all the things," + 
+	        	" I wanted us to overcome.</p>" + 
+	        	"<p>As I remember, as I remember we were ignorant once, but kids all the same And do you remember?" + 
+	        	" Do you remember waking up to a world wholly new, or did you resist every change?</p>" + 
+	        	"<p>Somehow - STILL HERE Another wasted year. This ship is going down, " + 
+	        	"but by myself I'll bail it out. Reflect. Implode. Just skip it and more on. " + 
+	        	"This ship is going down, and together we'll fucking drown.</p>" + 
+	        	"<p>I thought there was no way I could stay, That my dreams had all been washed away. No tears or shades of gray, everything we had was gone. But there is plenty left to say; refuse to live my life that way Now fear ignorant days; when everything we built was lost.</p>" + 
+	        	"<p>This is a clean slate for me, and I won't let this go to waste. I'll say the thing's I have to say so, Fuck you - Fuck you - Fuck you This is a clean slate for me, and I know that I have you to thank. But you won't get credit for everything so, Thank you - Thank you - Fuck you</p>" + 
+	        	"<p>This ship is still going down, but I will not fucking drown</p>").fadeIn(1000);
+			});
+    });
+    $('#mp-btn').click(function() {
+		$('#mid-music').fadeOut(180, function(){
+        	$('#mid-music').empty().append("<h1>Moments Passed</h1>" + 
+        		"<p>We’re under attack, but most of us are too numb to know." + 
+        		" Open your eyes really look at who you are. Do you like being so naïve? " + 
+        		"Taking every word as fact. You’ve got to use your head, before your moments passed.</p>" + 
+        		"<p>This is your life - I know who you are - And your better than them all. " + 
+        		"This is our time - Step up and take a stand - It may be your last chance.</p>" + 
+        		"<p>Our spirit remains, defined by a power they’ll never know. Always getting stronger, " + 
+        		"more resistant to attack. This fight will be long and hard, and filled with sweat and blood. " + 
+        		"We’ve got to hold our ground never broken, can’t be done.</p>" +
+        		"<p>Our spirit remains, defined by a power they’ll never know. And we will hold our ground, " + 
+        		"never broken, can’t be done.</p>" +
+        		"<p>This is your life - Don’t let it go to waste - We all rise and fall - " + 
+        		"What you need you have to take - Make it your best</p>" +
+        		"<pYour brain has a filter . I know you’re smart . Think for yourself. " + 
+        		"Your brain has a filter - You’re fuckin’ smart - Just turn it on.p>" +
+        		"<p>Think-for-your-self - Take what you need - Take what you need</p>" +
+        		"<p>This is. Your life. Think fast.</p>").fadeIn(1000);
+    	});
+	});
+    $('#ny-btn').click(function() {
+        $('#mid-music').fadeOut(800, function(){
+        	$('#mid-music').empty().append("<h1>Next Year</h1>" + 
+        		"<p>Have you, had enough. Lying to yourself and everybody else.</p>" + 
+        		"<p>There's no way that I can trust; you've lost my respect and my faith. " + 
+        		"All alone and with everything, you're lucky all it's cost. " + 
+        		"My words can’t describe my thoughts.</p>" +
+        		"<p>Cause you're fake. You're nothing. You fucking make me sick.All the years of selling out; " + 
+        		"you better react quick.</p>" +
+        		"<p>You'll walk alone,  ONE DAY. I think we’re better off alone.</p>" +
+        		"<p>Everything that I believe. In better things I have not seen. " + 
+        		"I am here hello, I say goodbye.</p>" +
+        		"<p>What can I do to make my point, and what can I say to make you see. " + 
+        		"You need to start listening before it's too late and everyone's gone. " + 
+        		"Gone… Alone.... you'll be.</p>" +
+        		"<p>ou need to take it fucking take it, [but] it's not for you. " + 
+        		"This is the State of corporate tools</p>" +
+        		"<p>This time, that time, no time for you, realize you fucking lose. " + 
+        		"Hope this is a lesson for you, I don't have patience for you. " + 
+        		"I can’t believe your shit, I told you before and I'll tell you again. " + 
+        		"I am right here, motherfucker I'll be here next year.</p>").fadeIn(1000);
+        });    
+    });
+    $('#va-btn').click(function() {
+        $('#mid-music').fadeOut(800, function(){
+        	$('#mid-music').empty().append("<h1>Venting Anger</h1>" + 
+        		"<p>One call brings to an end restraints that held us back our dreams of better lives side by side.</p>" +
+        		"<p>Now I’m left to hold the slack All alone still just a kid this weight, " + 
+        		"my family torn apart; my job to mend.</p>" +
+        		"<p>I know I’m old enough to live the life you left behind and I know you’re old enough to do what’s right</p>" +
+        		"<p>Just come home. She needs you and so do I. " + 
+        		"Abandoned and alone kids struggling to get by</p>" +
+        		"<p>Go off and die with your selfish dreams don’t forget to think of home now that you’re free</p>" +
+        		"<p>On second thought times are best without all that you are selfishness and ugly pride</p>" +
+        		"<p>All the time spent, all the memories it’s all fake; just " + 
+        		"a walking dream I always thought you were a man but you’re just another deadbeat dad</p>" +
+        		"<p>It’s not fair to miss all of the work and skip right to the finish line. " + 
+        		"You’ve got to put in your time. So here’s your chance to make things right.</p>").fadeIn(1000);
+        });    
+    });
+    $('#lfy-btn').click(function() {
+        $('#mid-music').fadeOut(800, function(){
+        	$('#mid-music').empty().append("<h1>Live for Yourself</h1>" +
+        		"<p>Make a stand - for yourself - or Decide to be like everyone else. " + 
+        		"You've got to find your own path, find your - own path. " + 
+        		"We've got to find it in ourselves, not to be like everyone else. " + 
+        		"You've got to find your own path, find your - own path.</p>" +
+        		"<p>I won't allow myself to be pushed down, this time I'll walk alone. " + 
+        		"I won't allow myself to be washed out, this time I'll walk alone.</p>" +
+        		"<p>Alone.</p>" +
+        		"<p>Here I stand, a different man in these shoes. " + 
+        		"Forever changed deep down, I've found it in myself. " + 
+        		"This time it will be ok, forever changed deep down and the same with everyday.</p>" +
+        		"<p>You've got to make a stand, choose a path for yourself. " + 
+        		"Set goals and follow through, and set yourself apart.</p>" +
+        		"<p>You've got to keep on fighting, to find what's in your heart. " + 
+        		"You've got to keep on fighting, and set yourself apart.</p>" +
+        		"<p>Your Heart.</p>" +
+        		"<p>Live for yourself - live for tomorrow - It’s in your hands - it’s in your control " + 
+        		"- it’s up to you right now</p>" +
+        		"<p>Live, Live for yourself.</p>").fadeIn(1000);
+        });    
+    });
+    $('#ltf-btn').click(function() {
+        $('#mid-music').fadeOut(800, function(){
+        	$('#mid-music').empty().append("<h2>Light the Flame</h2>" + 
+        		"<p>Everything I have I would gladly give, so much you’ll never know This is my time, " + 
+        		"this is my life, it’s all I’ve ever known</p>" +
+        		"<p>If I could go back and change things I’d do it all the same. " + 
+        		"The struggles in life you have to overcome because (the struggle?) " + 
+        		"struggling is only part of the game</p>" +
+        		"<p>It’s the fight in our heart that defines us. It dictates who we are. " + 
+        		"Our struggles everyday set us apart Apart.</p>" +
+        		"<p>If I had a choice I would gladly give my soul in hope for change</p>" +
+        		"<p>There’s pressure to give in, but we can’t succeed alone Innocence is gone. " + 
+        		"With nothing left  but home. There’s progress in these streets...dripping with disdain. " + 
+        		"As there comes a time, in your life when you have to, you have to break away.</p>" +
+        		"<p>The time is now. This is the way out</p>" +
+        		"<p>Yeah, we’re fighting, fighting for change Yeah, we’re lighting, lighting the flame</p>" +
+        		"<p>We’re trying to reshape the face of power in the States. " + 
+        		"Breaking down the arguments used to segregate Humans from ourselves and power just the same. " + 
+        		"It’s on us to light the flame.</p>" + 
+        		"<p>Light the flame.</p>").fadeIn(1000);
+        });    
+    });
+    $('#fn-btn').click(function() {
+        $('#mid-music').fadeOut(800, function(){
+        	$('#mid-music').empty().append("<h2>False Narratives</h2>" +
+        		"<p>I am so confused by all the systems around me. " + 
+        		"Where power defines all, individuals get their gains. " + 
+        		"Disconnected, we're vicious and ugly we follow every move, dreaming of our day</p>" +
+        		"<p>and I can't understand the things I see; " + 
+        		"they all seem so inhumane when I've never meant to hurt anyone, anyone but me. " + 
+        		"So fly your drones and pick your words, but I can think and I will fight. " + 
+        		"No longer just the walking dead, Resistance starts with me</p>" +
+        		"<pBecause in our name we kill and maim, self-proclaimed innocence in distance. " + 
+        		"And with our heads held high we reject equality at every turn; humanity betrayed " + 
+        		"to protect what's 'mine'<p>" +
+        		"<p>Because you're right, there IS a problem When a man is kept down by another man's hand with a gun.</p>" +
+        		"<p>But it is even more obvious now that WE hold the loaded guns. Our avoidance and neglect, " + 
+        		"our vicious competition, Exceptionalism in every drop of sweat</p>" +
+        		"<p>That there is more to life than security. Than these false black and white narratives. " + 
+        		"Life exists in the space between with grays brilliant as the sun</p>" +
+        		"<p>There is more to life than security</p>").fadeIn(1000);
+        });    
+    });
+});
